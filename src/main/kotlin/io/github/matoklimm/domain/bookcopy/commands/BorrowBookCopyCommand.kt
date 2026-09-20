3053adf4ed2a00a4db9ca762dca638f3d3948aa2
Klimm-Max <klimm.max@gmail.com>
2026-09-20 18:26:24 +0200

    formatting

--- src/main/kotlin/io/github/matoklimm/domain/bookcopy/commands/BorrowBookCopyCommand.kt
+++ src/main/kotlin/io/github/matoklimm/domain/bookcopy/commands/BorrowBookCopyCommand.kt
@@ -4,5 +4,4 @@ import io.github.matoklimm.domain.bookcopy.BookCopyId
 import java.time.LocalDate
 
 // FIXME userId is String on purpose to keep things easy while we explore event sourcing
-data class BorrowBookCopyCommand(val bookCopyId: BookCopyId, val userId: String, val borrowedUntil: LocalDate? = null) :
-    BookCopyCommand
+data class BorrowBookCopyCommand(val bookCopyId: BookCopyId, val userId: String, val borrowedUntil: LocalDate? = null) : BookCopyCommand
