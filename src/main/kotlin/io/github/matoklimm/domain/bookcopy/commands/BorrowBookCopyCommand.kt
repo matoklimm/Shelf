f7366c3f7245191345db16f5d6ef6c97cac766f8
Klimm-Max <klimm.max@gmail.com>
2026-09-20 17:16:06 +0200

    using default variable for borrowedUntil which is more convenient

--- src/main/kotlin/io/github/matoklimm/domain/bookcopy/commands/BorrowBookCopyCommand.kt
+++ src/main/kotlin/io/github/matoklimm/domain/bookcopy/commands/BorrowBookCopyCommand.kt
@@ -4,5 +4,5 @@ import io.github.matoklimm.domain.bookcopy.BookCopyId
 import java.time.LocalDate
 
 // FIXME userId is String on purpose to keep things easy while we explore event sourcing
-data class BorrowBookCopyCommand(val bookCopyId: BookCopyId, val userId: String, val borrowedUntil: LocalDate?) :
+data class BorrowBookCopyCommand(val bookCopyId: BookCopyId, val userId: String, val borrowedUntil: LocalDate? = null) :
     BookCopyCommand
